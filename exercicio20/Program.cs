b5b4a5c01cf3ce8db6a28c83586f5bac6ffb8fb1
Adriana Garibalde <adrianagaribalde@gmail.com>
2022-10-06 21:07:10 -0300

    exercicio20 - Conta Corrente2

---
+++ exercicio20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercicio20 {
+    class Program {
+        static void Main(string[] args) {
+            ContaCorrente2 c1 = new ContaCorrente2(100, 'D');
+            c1.ImpressaoTransESaldo();
+
+            ContaCorrente2 c2 = new ContaCorrente2(1000, 'L');
+            c2.ImpressaoTransESaldo();
+
+            ContaCorrente2 c3 = new ContaCorrente2(200, 'D');
+            c3.ImpressaoTransESaldo();
+
+            ContaCorrente2 c4 = new ContaCorrente2(50, 'L');
+            c4.ImpressaoTransESaldo();
+        }
+    }
+}
