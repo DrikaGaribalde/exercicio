fae109a86709289214d3a33bd7533f8bb65c3351
Adriana Garibalde <adrianagaribalde@gmail.com>
2022-10-10 19:59:08 -0300

    exercicio21_SobreposicaoDeMetodos

---
+++ exercicio21/Amigo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio21 {
+    class Amigo {
+        private string Nome;
+        private string Apelido;
+
+        public Amigo(string N, string A) {
+            Nome = N;
+            Apelido = A;
+        }
+
+        public override string ToString() {
+            return Apelido + ", " + Nome;
+        }
+    }
+}
